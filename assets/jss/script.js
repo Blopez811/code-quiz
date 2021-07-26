3af2743f0796691b3cd3aabb2bd1021af9fed473
Benicio Lopez <benicio.lopez@gmail.com>
2021-07-25 20:56:03 -0500

    added feature saves scores to local storage

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -5,15 +5,23 @@ var answers = document.querySelectorAll(".answer");
 var question2 = document.querySelector("#question2");
 var question3 = document.querySelector("#question3");
 var finalScore = document.querySelector("#finalScore");
+var playerScore = localStorage.getItem("scores");
+if (!playerScore) {
+    playerScore = [];
+}
+else {
+    playerScore = JSON.parse(playerScore);
+}
 var timerEl = document.querySelector("#timer");
 var timeLeft = 75;
 var finalScoreText = document.querySelector("#finalScoreText");
-
+var playerInput = document.querySelector("#initials");
+// var playerScore = playerInput.value + timeLeft
 var startQuiz = function () {
     quizInfo.classList.add("hide");
     question1.classList.remove("hide");
-    var downloadTimer = setInterval(function(){
-        if(timeLeft <=0) {
+    var downloadTimer = setInterval(function () {
+        if (timeLeft <= 0) {
             clearInterval(downloadTimer);
             question1.classList.add("hide");
             question2.classList.add("hide");
@@ -21,13 +29,13 @@ var startQuiz = function () {
             finalScore.classList.remove("hide");
             timerEl.classList.add("hide");
             document.querySelector("#timer").innerHTML = 0;
-            document.querySelector("#finalScoreText").innerHTML= "You're score is " + 0 + "!";
+            document.querySelector("#finalScoreText").innerHTML = "You're score is " + 0 + "!";
         }
         else {
-            document.querySelector("#timer").innerHTML = timeLeft; 
+            document.querySelector("#timer").innerHTML = timeLeft;
         }
-        timeLeft -= 1; 
-        }, 1000);
+        timeLeft -= 1;
+    }, 1000);
 }
 
 var answeredQuestion = function () {
@@ -41,42 +49,42 @@ var answeredQuestion = function () {
     }
 }
 
-var changeQuestion = function() {
+var changeQuestion = function () {
     question1.classList.add("hide");
     question2.classList.remove("hide")
 }
 
 start.addEventListener("click", startQuiz);
 
-for(var i = 0; i < 4; i++){
-   var answer = answers[i];
+for (var i = 0; i < 4; i++) {
+    var answer = answers[i];
     answer.addEventListener("click", answeredQuestion);
     answer.addEventListener("click", changeQuestion);
 };
 
-var answeredQuestion2 = function() {
-    if(this.id  == "b6") {
+var answeredQuestion2 = function () {
+    if (this.id == "b6") {
         console.log("Correct!");
     }
-    else  {
+    else {
         console.log("Wrong 2!");
-        timeLeft = timeLeft -10;
+        timeLeft = timeLeft - 10;
     }
 }
 
-var changeQuestion2 = function() {
+var changeQuestion2 = function () {
     question2.classList.add("hide");
     question3.classList.remove("hide");
 }
 
-for(var i = 4; i < 8; i++){
+for (var i = 4; i < 8; i++) {
     var answer = answers[i];
-     answer.addEventListener("click", answeredQuestion2);
-     answer.addEventListener("click", changeQuestion2);
- };
+    answer.addEventListener("click", answeredQuestion2);
+    answer.addEventListener("click", changeQuestion2);
+};
 
-var answeredQuestion3 = function() {
-    if(this.id == "b11") {
+var answeredQuestion3 = function () {
+    if (this.id == "b11") {
         console.log("Correct!")
     }
     else {
@@ -85,14 +93,22 @@ var answeredQuestion3 = function() {
     }
 }
 
-var changeQuestion3 = function() {
+var changeQuestion3 = function () {
     question3.classList.add("hide");
     finalScore.classList.remove("hide");
     finalScoreText.innerHTML = "You're score is " + timeLeft + "!";
     timerEl.classList.add("hide");
 }
-for(var i = 8; i < 12; i++){
+for (var i = 8; i < 12; i++) {
     var answer = answers[i];
-     answer.addEventListener("click", answeredQuestion3);
-     answer.addEventListener("click", changeQuestion3);
- };
+    answer.addEventListener("click", answeredQuestion3);
+    answer.addEventListener("click", changeQuestion3);
+};
+
+submitScore = function () {
+    event.preventDefault();
+    var playerInput = document.querySelector("#initials");
+    playerScore.push(playerInput.value + timeLeft);
+    localStorage.setItem("scores", JSON.stringify(playerScore));
+}
+document.querySelector("#submit").addEventListener("click", submitScore)
